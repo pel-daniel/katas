5f29e7bd8f3f7cb1d4656c01322d2845972438f5
DanielGarcia <pel_daniel@hotmail.com>
2013-04-23 04:34:32 -0500

    Refactor with arrays

---
+++ BowlingGameKata/BowlingGameKata/Frame.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+
+namespace BowlingGameKata
+{
+    internal class Frame
+    {
+        private const int Strike = 10;
+        private int[] rolls = new int[2];
+        private int rollIndex = 0;
+
+        private bool IsSpare()
+        {
+            return Score() == 10;
+        }
+
+        public bool IsStrike()
+        {
+            return rolls.First() == Strike;
+        }
+
+        public bool HasTwoRolls()
+        {
+            return rollIndex == 2;
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[rollIndex++] = pins;
+        }
+
+        public int Score()
+        {
+            return rolls.Sum();
+        }
+
+        public int NumberOfBonusRolls()
+        {
+            var bonusRolls = 0;
+
+            if (IsStrike())
+                bonusRolls = 2;
+            else if (IsSpare())
+                bonusRolls = 1;
+
+            return bonusRolls;
+        }
+    }
+}
--- BowlingGameKata/BowlingGameKata/Game.cs
+++ BowlingGameKata/BowlingGameKata/Game.cs
@@ -2,64 +2,16 @@
 {
     public class Game
     {
-        private int[] rolls = new int[21];
-        private int currentRoll = 0;
+        private Frames frames = new Frames();
 
         public void Roll(int pins)
         {
-            rolls[currentRoll++] = pins;
+            frames.Roll(pins);
         }
 
         public int Score()
         {
-            var score = 0;
-            var frameIndex = 0;
-
-            for (var frame = 0; frame < 10; frame++)
-            {
-                if (IsStrike(frameIndex))
-                {
-                    score += 10 + StrikeBonus(frameIndex);
-                    frameIndex++;
-                }
-                else if (IsSpare(frameIndex)) 
-                {
-                    score += 10 + SpareBonus(frameIndex);
-                    frameIndex += 2;
-                }
-                else
-                {
-                    score += SumOfBallsInFrame(frameIndex);
-                    frameIndex += 2;
-                }
-            }
-
-            return score;
-        }
-
-        private int SpareBonus(int frameIndex)
-        {
-            return rolls[frameIndex + 2];
-        }
-
-        private int SumOfBallsInFrame(int frameIndex)
-        {
-            return rolls[frameIndex] + rolls[frameIndex + 1];
-        }
-
-        private int StrikeBonus(int frameIndex)
-        {
-            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
-        }
-
-        private bool IsStrike(int frameIndex)
-        {
-            return rolls[frameIndex] == 10;
-        }
-
-        private bool IsSpare(int frameIndex)
-        {
-            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+            return frames.Score();
         }
     }
 }
