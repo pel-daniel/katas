ab7d6249a776a24952fc9e43c7b2cac3d88437c7
DanielGarcia <pel_daniel@hotmail.com>
2013-04-23 02:55:57 -0500

    3 test: roll spare

--- BowlingGameKata/BowlingGameKata/BowlingGameTest.cs
+++ BowlingGameKata/BowlingGameKata/BowlingGameTest.cs
@@ -12,9 +12,14 @@ namespace BowlingGameKata
     {
         Game g;
 
-        private void RollMany(int n, int pins)
+        [TestInitialize]
+        public void SetUp()
         {
             g = new Game();
+        }
+
+        private void RollMany(int n, int pins)
+        {
             for (var i = 0; i < n; i++)
                 g.Roll(pins);
         }
@@ -23,15 +28,30 @@ namespace BowlingGameKata
         public void TestGutterGame()
         {
             RollMany(20, 0);
-            Assert.AreEqual(0, g.Score);
+            Assert.AreEqual(0, g.Score());
         }
 
         [TestMethod]
         public void TestAllOnes()
         {
             RollMany(20, 1);
-            Assert.AreEqual(20, g.Score);
+            Assert.AreEqual(20, g.Score());
         }
 
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            RollSpare();
+            g.Roll(3);
+            RollMany(17, 0);
+
+            Assert.AreEqual(16, g.Score());
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+        }
     }
 }
--- BowlingGameKata/BowlingGameKata/Game.cs
+++ BowlingGameKata/BowlingGameKata/Game.cs
@@ -2,11 +2,39 @@
 {
     public class Game
     {
-        public int Score { get; private set; }
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
 
         public void Roll(int pins)
         {
-            Score += pins;
+            rolls[currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            var score = 0;
+            var frameIndex = 0;
+
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsSpare(frameIndex)) // Spare
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
         }
     }
 }
